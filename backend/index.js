61e2850a52ff03f3170097277411d958ac473d1f
imnhaphuong <phuongptn.236@gmail.com>
2022-11-20 15:59:02 +0700

    fix build server res404

---
+++ backend/index.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const mongoose = require("mongoose");
+
+app.use(express.json());
+
+//call API
+const userAPI = require("./routes/userRoutes");
+app.use("/api/users", userAPI);
+app.use("/", (req, res) => {
+  res.send({
+    status: "success",
+  });
+});
+
+//Connect mongodb
+const mongoUri = `mongodb+srv://nhaphuong:!2345@cluster0.nepewkn.mongodb.net/TellMe?retryWrites=true&w=majority`;
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+//Check connection
+mongoose.connection.on("connected", () => {
+  console.log("Yehhh, congratulation! Connected with Mongo");
+});
+mongoose.connection.on("error", (err) => {
+  console.log("Has an error when connect with Mongo", err);
+});
+
+app.listen(port, () => {
+  console.log(
+    `server is running example is listening at http://localhost:${port}`
+  );
+});
