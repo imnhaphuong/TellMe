7ed5ec039586a6fd04e27af150cc246f735962ce
LinhTrang <trangnguyen24201@gmail.com>
2022-11-22 15:35:24 +0700

    fix app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,14 @@
-import { useState } from "react";
+// import { useState } from "react";
+import Conversation from "./containers/client/Conversation/Conversation";
+import Client from "./containers/client/Client.jsx";
+// import { BrowserRouter, Router, Route, Switch } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Router> */}
+          <Conversation/>
+      {/* </Router> */}
     </div>
   );
 }
