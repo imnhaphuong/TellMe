95211bd2c570d4b6bff3d6f33727bac5bc3c9246
imnhaphuong <phuongptn.236@gmail.com>
2022-11-22 09:55:26 +0700

    config agora

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,5 @@
-import { Button } from "@material-ui/core";
 import { useState } from "react";
-import VideoCall from "./videocall/VideoCall"
 const App = () => {
-  const [inCall, setInCall] = useState(false);
-
-  // const client = useClient();
-  // const { ready, tracks } = useMicrophoneAndCameraTracks();
-  return (
-    <div style={{height: "100vh"}}>
-    <Button variant="contained" color="primary" onClick={()=>setInCall(true)} >Join</Button>
-      
-      {inCall ?<VideoCall setInCall={setInCall}/> : "Waiting" }
-      </div>
-    // ready && (
-    //   <AgoraVideoPlayer
-    //     videoTrack={tracks[1]}
-    //     style={{ height: "100%", width: "100%" }}
-    //   />
-    // )
-  );
+  return <div>abc</div>;
 };
 export default App;
