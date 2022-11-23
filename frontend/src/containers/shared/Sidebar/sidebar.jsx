52c59926cb8d39a835a92f881923b50351ce0845
quynhnga4200 <20211tt4200@mail.tdc.edu.vn>
2022-11-23 23:09:28 +0700

    completed Sidebar UI

--- frontend/src/containers/shared/Sidebar/sidebar.jsx
+++ frontend/src/containers/shared/Sidebar/sidebar.jsx
@@ -1,33 +1,36 @@
 import { BsStarFill, BsFileEarmarkTextFill, BsPeopleFill, BsFillBellFill, BsMoon } from "react-icons/bs";
 import { AiFillSetting, AiOutlinePoweroff } from "react-icons/ai";
+import { memo, useState } from "react";
 
 const Sidebar = () => {
-    const Menus = [
-        { title: "Status", icon: <BsStarFill /> },
-        { title: "Favourite", icon: <BsFileEarmarkTextFill /> },
-        { title: "Contact", icon: <BsPeopleFill /> },
-        { title: "Notification", icon: <BsFillBellFill /> },
-        { title: "Settings", icon: <AiFillSetting /> },
-        { title: "Change Mode", icon: <BsMoon /> },
-        { title: "Signout", icon: <AiOutlinePoweroff /> },
+    const [current, setCurrent] = useState(0);
+    const menus = [
+        { title: "Favourite", icon: <BsStarFill />, page: <span>Favourite</span>, selectedIcon: <BsStarFill className="fill-white" /> },
+        { title: "Documents", icon: <BsFileEarmarkTextFill />, page: <span>Documents</span>, selectedIcon: <BsFileEarmarkTextFill className="fill-white" /> },
+        { title: "Contact", icon: <BsPeopleFill />, page: <span>Contact</span>, selectedIcon: <BsPeopleFill className="fill-white" /> },
+        { title: "Notification", icon: <BsFillBellFill />, page: <span>Notification</span>, selectedIcon: <BsFillBellFill className="fill-white" /> },
+        { title: "Settings", icon: <AiFillSetting />, page: <span>Settings</span>, selectedIcon: <AiFillSetting className="fill-white" /> },
+        { title: "Change Mode", icon: <BsMoon />, page: <span>Change Mode</span>, selectedIcon: <BsMoon className="fill-white" /> },
+        { title: "Signout", icon: <AiOutlinePoweroff />, page: <span>Signout</span>, selectedIcon: <AiOutlinePoweroff className="fill-white" /> },
     ]
     return (
-        <div class="flex">
-            <div class="h-screen border">
-                <div class="p-7 border">
+        <div className="flex">
+            <div className="h-screen border">
+                <div className="p-7 border">
                     <img src={require('../../../asset/image/avatar.jpg')} width="50" height="50" />
                 </div>
-                <div class="p-7 ">
+                <div className="p-7 ">
                     <div>
-                        <img src={require('../../../asset/image/avatar.jpg')} width="50" height="50" class="border-4 border-blue-700 rounded-full" />
+                        <img src={require('../../../asset/image/avatar.jpg')} width="50" height="50" className="border-4 border-blue-700 rounded-full" />
                     </div>
-                    <div class="pt-7 flex items-center flex flex-col space-y-12 text-3xl ">
-                        {Menus.map((menu, index) => (
+                    <div className="pt-8 flex items-center flex flex-col space-y-12">
+                        {menus.map((menu, index) => (
                             <>
-                                <div key={index}>
-                                    <div class="bg-light-gray rounded-full hover/item:bg-dark-gray" onClick={()=> {
-                                        
-                                    }}>{menu.icon}</div>
+                                <div key={index} className={`rounded-full hover/item:bg-dark-gray p-4 cursor-pointer ${current === index ? "bg-blue-700" : "bg-light-gray"}`} onClick={() => {
+                                    setCurrent(index)
+                                }}>
+                                    <div >{current === index ? menu.selectedIcon : menu.icon}</div>
+
                                 </div>
                             </>
                         )
@@ -35,11 +38,10 @@ const Sidebar = () => {
                     </div>
                 </div>
             </div >
-            <div>
-                <h1>Page</h1>
+            <div className="h-screen border">
+                {menus[current].page}
             </div>
         </div>
-
     )
 }
-export default Sidebar;
+export default memo(Sidebar);
