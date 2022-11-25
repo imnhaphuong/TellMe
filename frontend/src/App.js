6053ee07eac07cca6043ed454835f7a50fb543b0
LinhTrang <trangnguyen24201@gmail.com>
2022-11-25 22:26:12 +0700

    fix conflict

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 // import { useState } from "react";
 import Conversation from "./containers/client/Conversation/Conversation";
 import Client from "./containers/client/Client.jsx";
+import Signup from "./containers/client/Singup/Signup";
+
 // import { BrowserRouter, Router, Route, Switch } from "react-router-dom";
 import Signup from "./containers/client/Singup/Signup";
 import Login from "./containers/client/Login/Login";
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,14 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  important: true,
   content: ["./src/**/*.{js,jsx}"],
   theme: {
     //custom
     extend: {
+      fontFamily: {
+        'worksans': ['Work Sans'],
+      },
+      extend: {
+        spacing: {
+          '25px': '25px',
+        },
+        boxShadow: {
+          'xl': '-1px 10px 16px -10px #1c9dea',
+        }
+      },
       colors: {
+        'success': '#3fcc35',
+        'primary': '##1c9dea',
+        'error': '#ff4e2b ',
         "light-gray" : "#F5F5F5",
         "dark-gray": "#E8E8E8",
       },
     },
-  },
-  plugins: [],
+    plugins: [],
+    corePlugins: {
+      preflight: false,
+    },
+  }
 }
