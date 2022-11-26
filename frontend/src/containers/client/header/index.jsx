31ca11bff95f847a1981f171a4925f4ece79ed45
quynhnga4200 <20211tt4200@mail.tdc.edu.vn>
2022-11-26 15:01:01 +0700

    edit dheader

--- frontend/src/containers/client/header/index.jsx
+++ frontend/src/containers/client/header/index.jsx
@@ -2,15 +2,17 @@ import React from "react"
 
 const Welcome = () => {
     return (
-        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2">
-            <div className="mx-1/3">
-                <img src="https://cdn.logo.com/hotlink-ok/logo-social.png" width="200" height="200" />
-                <h1>TELL ME</h1>
-                <h2>Hello Everyone, WE are TellMe</h2>
-                <span>Welcome to tellme</span>
+        <div className="grid">
+            <div className="flex items-center">
+                <img src="https://cdn.logo.com/hotlink-ok/logo-social.png" width="200" height="200"/>
+                <p className="text-4xl font-mono">TELL ME</p>
+            </div>
+            <div>
+                <h2 className="text-larg font-mono">Hello Everyone, WE are TellMe</h2>
+                <span className="text-sm text-slate-400 font-mono">Welcome to tellme</span>
             </div>
         </div>
-        
+
     )
 
 }
