32e2f640ab381f5ee7c38e1b0cc97506a81a2e21
LinhTrang <trangnguyen24201@gmail.com>
2022-12-17 16:29:06 +0700

    fix converApi.js

---
+++ frontend/src/apis/converApi.js
@@ -0,0 +1,27 @@
+
+import axios from 'axios';
+
+const converApi = {
+  getConverByUserAPI(owner) {
+    return axios({
+      url: `http://localhost:8080/api/convers/userId/${owner}`,
+      method: 'get',
+    });
+
+  },
+  getAllConverAPI() {
+    return axios({
+      url: `http://localhost:8080/api/convers/`,
+      method: 'get',
+    });
+
+  },
+  getConverByIdAPI(converId) {
+    return axios({
+      url: `http://localhost:8080/api/convers/${converId}`,
+      method: 'get',
+    });
+
+  },
+}
+export default converApi;
